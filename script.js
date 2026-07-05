ce0f8eb19b924a1a9e264d82d17d045f903799e8
jameswaweru188-beep <jameswaweru188@gmail.com>
2026-07-05 11:51:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const celebrateButton = document.getElementById("celebrateButton");
+const surprise = document.getElementById("surprise");
+
+celebrateButton.addEventListener("click", () => {
+surprise.textContent = "🎊 May your day be full of joy, laughter, cake, and love! 🎊";
+});
